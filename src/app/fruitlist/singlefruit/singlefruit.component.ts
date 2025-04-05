bb85719de4778f70ff747233a383f0c2b2914bb1
Alessandro Argenziano <1603alessandro@gmail.com>
2025-04-05 12:58:54 +0200

    added an [(ngModel)], tested the input field

--- src/app/fruitlist/singlefruit/singlefruit.component.ts
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-singlefruit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './singlefruit.component.html',
   styleUrl: './singlefruit.component.scss',
 })
@@ -21,8 +22,11 @@ export class SinglefruitComponent {
     ],
   };
 
+  inputData = '';
+
   @Output() fruitname = new EventEmitter<string>();
-  emitName() {
-    this.fruitname.emit(this.fruit.name);
+  sendInputData() {
+    // this.fruitname.emit(this.fruit.name);
+    console.log(this.inputData);
   }
 }
